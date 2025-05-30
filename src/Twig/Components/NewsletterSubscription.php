96dc9afec3708bed96431e7687dc1737f5d36416
Charles <charles@charlesen.fr>
2025-05-30 12:22:50 +0200

    Feat : update daisyui version

---
+++ src/Twig/Components/NewsletterSubscription.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Twig\Components;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent('NewsletterSubscription')]
+class NewsletterSubscription
+{
+    // Vous pouvez ajouter des propriétés et des méthodes ici si nécessaire
+    // par exemple pour injecter des services ou des paramètres
+}
